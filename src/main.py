70f893b011074a4300051d932944a627b11f4ccd
cc-jj <gaspercj@gmail.com>
2022-02-03 01:20:57 -0500

    Fix CORS issue causing Network Errors on 500 Internal Server Errors; add initial logging

--- src/main.py
+++ src/main.py
@@ -1,10 +1,49 @@
-from fastapi import FastAPI
+import logging
+import sys
+import traceback
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 
-from src import routes
+from src import routes, settings
+
+logger = logging.getLogger("bakery")
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+def startup():
+    logging.basicConfig(level=logging.INFO)
+    if settings.DEBUG_MODE:
+        logger.setLevel(logging.DEBUG)
+    if logger.isEnabledFor(logging.INFO):
+        pretty_settings = (f"{k.lower():<25} {v}" for k, v in sorted(settings.dump().items()))
+        logger.info("Settings\n%s", "\n".join(pretty_settings))
+
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    """
+    Ensure CORS headers are added to response when an unhandled exception occurs:
+    https://github.com/tiangolo/fastapi/issues/775#issuecomment-592946834
+    """
+    try:
+        return await call_next(request)
+    except Exception:
+        logger.exception("Unhandled exception")
+        content = {"detail": "Internal server error"}
+        if settings.DEBUG_MODE:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            tb = traceback.format_exception(exc_type, exc_value, exc_traceback, limit=25)
+            content.update(traceback=tb)
+        return JSONResponse(content, status_code=500)
+
+
+app.middleware("http")(catch_exceptions_middleware)
+
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:8080"],
--- src/settings.py
+++ src/settings.py
@@ -4,4 +4,14 @@ JWT_SECRET = os.environ["JWT_SECRET"]
 JWT_ALGO = "HS256"
 JWT_TIMEOUT_MINUTES = int(os.getenv("JWT_TIMEOUT_MINUTES", 60))
 
+# Controls logging level and enables including traceback in 500 InternalServerError responses
+DEBUG_MODE = os.getenv("DEBUG") == "1"
+
 SQLALCHEMY_DATABASE_URL = os.environ["SQLALCHEMY_DATABASE_URL"]
+
+
+def dump():
+    return {
+        "DEBUG_MODE": DEBUG_MODE,
+        "JWT_TIMEOUT_MINUTES": JWT_TIMEOUT_MINUTES,
+    }
